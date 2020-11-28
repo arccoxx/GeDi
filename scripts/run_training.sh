e486e23583963c110a4ad77be61aff073fb01d09
arccoxx <arcco96@gmail.com>
2020-11-27 19:44:12 -0500

    generalized training script
    generalized the training script now it will run on the /data directory as opposed the previous /data/AG-news directory

--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -9,18 +9,18 @@ python ../train_GeDi.py --task_name SST-2 \
   --do_eval  \
   --do_train \
   --logit_scale \
-  --data_dir ../data/AG-news  \
-  --max_seq_length 192 \
+  --data_dir ../data  \
+  --max_seq_length 1024 \
   --overwrite_cache \
-  --per_gpu_train_batch_size 4 \
-  --per_gpu_eval_batch_size  8 \
+  --per_gpu_train_batch_size 1 \
+  --per_gpu_eval_batch_size  1 \
   --learning_rate $lr  \
   --num_train_epochs 1.0  \
   --output_dir ../topic_GeDi_retrained \
   --model_type gpt2  \
   --model_name_or_path gpt2-medium \
   --gen_weight $lambda_ \
-  --logging_steps 500 \
+  --logging_steps 100 \
   --save_steps 5000000000 \
   --code_0 false \
   --code_1 true \
